01c8e2a4760a57467df52daae08a6d5e1fa0080e
BlaineBeltran <bbeltran1998@yahoo.com>
2023-08-18 09:19:11 -0500

    setup fastlane

---
+++ Concentration/Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Blaine Beltran on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
